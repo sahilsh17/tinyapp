d9403d2f00e3acd7590a6c27e721581a8bfbd6bb
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-12 23:18:03 +0000

    function getUserByEmail is moved to this file

---
+++ helper.js
@@ -0,0 +1,10 @@
+//looks if user object already has an email
+const getUserByEmail = function(email, users) {
+  for(let user in users) {
+    if(users[user].email === email) {
+      return user;
+    } 
+  }
+  
+}
+module.exports = {getUserByEmail};
