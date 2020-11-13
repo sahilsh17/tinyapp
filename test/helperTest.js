7e8e7a917648fbce118d79e99c5569d8e12a7ba3
Sahil Sharma <sahil.sharma237333@gmail.com>
2020-11-13 03:48:17 +0000

    eslint done

--- test/helperTest.js
+++ test/helperTest.js
@@ -3,10 +3,10 @@ const { assert } = require('chai');
 const { getUserByEmail } = require('../helper');
 console.log("aaa");
 const users = {
-  '9dgwzx': 
+  '9dgwzx':
    { id: '9dgwzx',
      email: 'b@b.com',
-     hashPassword: '$2b$10$HoRuEfjVc6yVYW6BUceTfeZyAtyI4oe1SwHR5l3o/9BUII2cK..BK' }, 
+     hashPassword: '$2b$10$HoRuEfjVc6yVYW6BUceTfeZyAtyI4oe1SwHR5l3o/9BUII2cK..BK' },
   'rh6w7h':
    { id: 'rh6w7h',
      email: 'c@c.com' }
@@ -14,19 +14,19 @@ const users = {
 };
 describe('getUserByEmail', function() {
   it('should return a user with valid email', function() {
-    const user = getUserByEmail("b@b.com", users)
+    const user = getUserByEmail("b@b.com", users);
     const expectedOutput = "9dgwzx";
     // Write your assert statement here
     
-     assert.equal(user, expectedOutput);
+    assert.equal(user, expectedOutput);
   });
 });
 describe('getUserByEmail', function() {
   it('should return a undefined for non-existent email', function() {
-    const user = getUserByEmail("d@d.com", users)
+    const user = getUserByEmail("d@d.com", users);
     const expectedOutput = undefined;
     // Write your assert statement here
     
-     assert.equal(user, expectedOutput);
+    assert.equal(user, expectedOutput);
   });
 });
